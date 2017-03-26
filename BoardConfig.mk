c315ded5c70ff7460434bd302af0b4e5242b762e
Rashed Abdel-Tawab <rashed@linux.com>
2017-03-26 15:45:13 -0400

    h872: Initial h872 tree

---
+++ BoardConfig.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/lge/h872
+
+# inherit from common g6
+-include device/lge/g6-common/BoardConfigCommon.mk
+
+TARGET_OTA_ASSERT_DEVICE := g6,lucye,h872
+
+# Kernel
+TARGET_KERNEL_CONFIG := lineageos_h872_defconfig
+
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+
+# inherit from the proprietary version
+-include vendor/lge/h872/BoardConfigVendor.mk
---
+++ lineage.mk
@@ -0,0 +1,15 @@
+$(call inherit-product, device/lge/h872/full_h872.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Overlays (inherit after vendor/cm to ensure we override it)
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+PRODUCT_NAME := lineage_h872
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="g6" \
+    PRODUCT_NAME="lucye_tmo_us" \
+    BUILD_FINGERPRINT="lge/lucye_tmo_us/lucye:7.0/NRD90U/17062223981e1:user/release-keys" \
+    PRIVATE_BUILD_DESC="lucye_tmo_us-user 7.0 NRD90U 17062223981e1 release-keys"
