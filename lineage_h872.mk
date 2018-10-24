a07e8663f15fd00dc7649c6ea431588001597211
Eric Meddaugh <eric@meddaughs.com>
2018-10-24 11:17:13 -0400

    h872: Consider lineage_h872 product via AOSP way
    Change-Id: I257fc13dc4c0fffab5cf1d483f7fcdb8c08a9217

--- lineage_h872.mk
+++ lineage_h872.mk
@@ -18,12 +18,23 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
 # Inherit from h872 device
 $(call inherit-product, device/lge/h872/device.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := h872
-PRODUCT_NAME := full_h872
+PRODUCT_NAME := lineage_h872
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-H872
 PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="lucye" \
+    PRODUCT_DEVICE="lucye" \
+    PRODUCT_NAME="lucye_tmo_us" \
+    PRIVATE_BUILD_DESC="lucye_tmo_us-user 7.0 NRD90U 17062223981e1 release-keys"
+
+BUILD_FINGERPRINT := "lge/lucye_tmo_us/lucye:7.0/NRD90U/17062223981e1:user/release-keys"
